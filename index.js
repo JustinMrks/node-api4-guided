fdb44f675e6bd878ad9f071b4109641e4fbdbfe5
justinmrks <justinmarks19@gmail.com>
2020-10-29 12:01:09 -0500

    env porting

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const server = require("./api/server.js");
 
-const port = 5000;
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+const secret = process.env.SECRET_THING || "I don't know about that";
+
+console.log(port, secret);
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
